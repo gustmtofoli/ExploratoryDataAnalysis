8f6bbeca00d4755972065ffdf8b7fecb1500179f
Gustavo Tófoli <gustavo.tofoli@objective.com.br>
2018-02-26 14:24:55 -0300

    pseudo facebook data

--- scripts/lessons.r
+++ scripts/lessons.r
@@ -14,3 +14,8 @@ stateSubsetBracket <- statesInfo[statesInfo$state.region == 1, ]
 stateSubsetBracket
 
 qplot(data = reddit, x = income.range)
+
+# ================================================
+
+pf <- read.csv('pseudo_facebook.tsv', sep = '\t')
+names(pf) # exibe todas as variáveis
