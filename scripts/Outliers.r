a05e8c22b59fcd3823d5b08613c62aedeee695bd
Gustavo Tófoli <gustavo.tofoli@objective.com.br>
2018-02-28 18:40:42 -0300

    obtendo outliers de somente um grupo

--- scripts/Outliers.r
+++ scripts/Outliers.r
@@ -3,3 +3,19 @@ outliers_values <- boxplot.stats(values$valor.procedimento)$out # get outliers
 outliers_values
 
 outliers_info <- subset(values, values$valor.procedimento == outliers_values)
+
+# =========================================================================
+
+# obter outliers de um grupo de valores já filtrado
+procedures <- read.csv('exportar_procedimento_executado.csv', sep = '\t')
+procedures_sub <- subset(procedures,procedures$CODIGO_PROCEDIMENTO == "40302423" & !is.null(procedures$VALOR_TOTAL))
+procedures_sub$VALOR_TOTAL
+
+n <- length(procedures_sub$VALOR_TOTAL) # quantidade de elementos no vetor
+
+index <- c(factor(1:n)) # cria vetor de 1 até quantidade de elementos no vetor
+
+outliers_values <- boxplot.stats(procedures_sub$VALOR_TOTAL)$out
+outliers_values
+
+# TODO agrupar grupos automaticamente
