6172bdf69086b583a6e49ba1bcb922d012dd77dd
Gustavo <gmtofoli@gmail.com>
2018-03-01 01:07:18 -0300

    agrupar e extrair outliers de cada um dos grupos

--- scripts/Outliers.r
+++ scripts/Outliers.r
@@ -6,7 +6,7 @@ for(cp in sort(unique(procedures$CODIGO_PROCEDIMENTO))) {
   procedures_subset <- subset(procedures, procedures$CODIGO_PROCEDIMENTO == cp)
   outliers <- boxplot.stats(procedures_subset$VALOR_TOTAL)$out
   if (length(outliers) != 0) {
-    count <- count + 1
+    procedures_with_outliers <- procedures_with_outliers + 1
     df_codproc_outliers <- data.frame(CODIGO_PROCEDIMENTO = c(cp), OUTLIERS = c(outliers))
     df_codproc_outliers_count <- count(df_codproc_outliers, c("CODIGO_PROCEDIMENTO", "OUTLIERS"))
     df_codproc_outliers_aggregate <- aggregate(df_codproc_outliers_count$freq, by = list(df_codproc_outliers_count$CODIGO_PROCEDIMENTO), FUN = sum)
