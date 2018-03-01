7089269aaa2aa1adb346965e406dc648678e42a0
Gustavo Tófoli <gustavo.tofoli@objective.com.br>
2018-03-01 18:15:56 -0300

    união de resultados em um dataframe

--- scripts/Outliers.r
+++ scripts/Outliers.r
@@ -1,16 +1,25 @@
 library(plyr)
 
-procedures_with_outliers = 0
+L_procedures <- c()
+L_values <- c()
+L_freq <- c()
 procedures <- read.csv('exportar_procedimento_executado.csv', sep = '\t')
 for(cp in sort(unique(procedures$CODIGO_PROCEDIMENTO))) {
   procedures_subset <- subset(procedures, procedures$CODIGO_PROCEDIMENTO == cp)
   outliers <- boxplot.stats(procedures_subset$VALOR_TOTAL)$out
   if (length(outliers) != 0) {
-    procedures_with_outliers <- procedures_with_outliers + 1
     df_codproc_outliers <- data.frame(CODIGO_PROCEDIMENTO = c(cp), OUTLIERS = c(outliers))
     df_codproc_outliers_count <- count(df_codproc_outliers, c("CODIGO_PROCEDIMENTO", "OUTLIERS"))
-    df_codproc_outliers_aggregate <- aggregate(df_codproc_outliers_count$freq, by = list(df_codproc_outliers_count$CODIGO_PROCEDIMENTO), FUN = sum)
-    print(df_codproc_outliers_count)
+    #df_codproc_outliers_aggregate <- aggregate(df_codproc_outliers_count$freq, by = list(df_codproc_outliers_count$CODIGO_PROCEDIMENTO), FUN = sum)
+    L_procedures <- c(L_procedures, df_codproc_outliers_count$CODIGO_PROCEDIMENTO)
+    L_values <- c(L_values, df_codproc_outliers_count$OUTLIERS)
+    L_freq <- c(L_freq, df_codproc_outliers_count$freq)
   }
 }
-procedures_with_outliers
+
+df_codproc_ouliers_freq <- data.frame(CODIGO_PROCEDIMENTO = L_procedures, 
+                                      OUTLIERS = L_values, 
+                                      FREQ = L_freq)
+df_codproc_ouliers_freq
+df_codproc_outliers_aggregate <- aggregate(df_codproc_ouliers_freq$FREQ, by = list(df_codproc_ouliers_freq$CODIGO_PROCEDIMENTO), FUN = sum)
+df_codproc_outliers_aggregate[order(df_codproc_outliers_aggregate$x, decreasing = TRUE), ]
