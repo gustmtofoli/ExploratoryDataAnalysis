03c0e2e501c66898b8d6b0864c524818ec152465
Gustavo Tófoli <gustavo.tofoli@objective.com.br>
2018-03-05 18:06:19 -0300

    gráfico número de amigos por gênero omitindo NA

--- scripts/lessons.r
+++ scripts/lessons.r
@@ -20,13 +20,32 @@ qplot(data = reddit, x = income.range)
 library(ggplot2)
 library(ggthemes)
 pf <- read.csv('pseudo_facebook.tsv', sep = '\t')
-names(pf) # exibe todas as variáveis
+
+# exibe todas as variáveis
+names(pf) 
+
+# gráficos para cada mês em função do dia do aniversário dos usuários
 qplot(data = pf, x = dob_day) +
-  scale_x_continuous(breaks = 1:31) +
+  scale_x_continuous(breaks = 1:31) + # quebra eixo x de 1 a 31 (dias do mês)
   facet_wrap(~dob_month, ncol = 4)
 
+# gráfico com intervalo no eixo x definido
 qplot(data = pf, x = friend_count, xlim = c(1,1000))
-
+# or
 qplot(data = pf, x = friend_count) +
   scale_x_continuous(limits = c(1, 1000))
 
+# gráfico com escala no eixo x de 50 em 50 e "colunas" quebrando de 25 em 25
+qplot(data = pf, x = friend_count, binwidth = 25) +
+  scale_x_continuous(limits = c(1, 1000), breaks = seq(0, 1000, 50))
+  
+# gráfico de número de amigos para cada gênero
+qplot(data = pf, x = friend_count, binwidth = 25) +
+  scale_x_continuous(limits = c(1, 1000), breaks = seq(0, 1000, 50)) +
+  facet_wrap(~gender)
+
+# gráfico de número de amigos para cada gênero, exceto NA
+qplot(data = subset(pf, !is.na(gender)), x = friend_count, binwidth = 25) +
+  scale_x_continuous(limits = c(1, 1000), breaks = seq(0, 1000, 50)) +
+  facet_wrap(~gender)
+  
