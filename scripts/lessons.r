a429e6b3cf8b1c31e8704fedf9941b91a52e07cb
Gustavo Tófoli <gustavo.tofoli@objective.com.br>
2018-03-08 18:02:40 -0300

    gráfico boxplot

--- scripts/lessons.r
+++ scripts/lessons.r
@@ -68,7 +68,7 @@ qplot(data = pf, x = tenure/365, binwidth = .25,
       color = I('black'), fill = I('#099DD9')) +
   scale_x_continuous(breaks = seq(1, 7, 1), limits = c(0, 7))
  
-# gráfico da idade dos usuários do facebook
+# gráfico da idade dos usuários do facebook com mudança de cor na borda e fill
 qplot(data = pf, x = age, binwidth = 1, color = I('#0D47A1'), fill = I('#2196F3'), 
       xlab = 'Age', ylab = 'Number of users in sample') +
   scale_x_continuous(breaks = seq(13, 113, 5), limits = c(13, 113))
@@ -91,3 +91,26 @@ grid.arrange(pl1, pl2, pl3)
 qplot(data = subset(pf, !is.na(pf$gender)), x = friend_count, binwidth = 10,
       geom = 'freqpoly', color = gender) +
   scale_x_continuous(breaks = seq(0, 1000, 50), limits = c(0, 1000))
+
+# mudando a escala do gráfico para obter mais detalhes
+qplot(data = subset(pf, !is.na(pf$gender)), x = www_likes, geom = 'freqpoly', color = gender) +
+  scale_x_continuous() +
+  scale_x_log10()
+
+# soma dos likes de agrupada por genero
+by(pf$www_likes, pf$gender, sum)
+
+# cria um gráfico boxplot di bpyneri de amigos por genero com correção das coordenadas cartesianas quando limita os dados
+qplot(data = subset(pf, !is.na(pf$gender)), x = gender, y = friend_count, geom = "boxplot") +
+  coord_cartesian(ylim = c(0, 250))
+
+# 75% das mulheres tem menos de 244 amigos (terceiro quartil)
+# 75% dos homens tem menos de 182 amigos (terceiro quartil)
+by(pf$friend_count, pf$gender, summary)
+
+# cria um gráfico boxplot do número de inicializçaões de amizade por genero
+qplot(data = subset(pf, !is.na(pf$gender)), x = gender, y = friendships_initiated, geom = "boxplot") +
+  coord_cartesian(ylim = c(0, 150))
+
+by(pf$friendships_initiated, pf$gender, summary)
+
