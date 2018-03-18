ecc8fbab43cbb99715802572907e8fd9b50e67a4
Gustavo <gmtofoli@gmail.com>
2018-03-18 04:06:20 -0300

    correlação

--- scripts/exploring_two_variables_lessons.r
+++ scripts/exploring_two_variables_lessons.r
@@ -42,9 +42,11 @@ pf.fc_by_age <- summarise(age_groups,
           friend_count_median = median(friend_count),
           n = n())
 
+# ordena por idade
 pf.fc_by_age <- arrange(pf.fc_by_age, age)
 
-head(pf.fc_by_age)
+# mostra 20 primeiras linhas
+head(pf.fc_by_age, 20)
 #or
 pf.fc_by_age_2 <- pf %>% 
   group_by(age) %>%
@@ -77,4 +79,43 @@ ggplot(data = pf, aes(x = age, y = friend_count)) +
   geom_line(stat = 'summary', fun.y = quantile, fun.args = list(probs = .1), linetype = 2, color = 'blue') +
   geom_line(stat = 'summary', fun.y = quantile, fun.args = list(probs = .5), color = 'blue') +
   geom_line(stat = 'summary', fun.y = quantile, fun.args = list(probs = .9), linetype = 2, color = 'blue')
+
+
+# CORRELAÇÃO
+
+# maior que 0.3 - significativa, mas pequena
+# 0.5 - moderada
+# 0.7 ou maior - grande
+
+?cor.test
+
+cor.test(pf$age, pf$friend_count, method = 'pearson')
+#or
+with(pf, cor.test(age, friend_count, method = 'pearson'))
+
+# retirando idade mais avançada, porque a probabilidade de serem dados incorretos é grande
+with(subset(pf, age <= 70), cor.test(age, friend_count, method = 'pearson'))
+
+# é imprudente dizer que a medida que a idade decresce o número de amigos tbm (relação negativa)
+# seria necessário usar estatística inferencial ao invés de estatística descritiva
+
+# utlizando a relação de spearman
+with(subset(pf, age <= 70), cor.test(age, friend_count, method = 'spearman'))
+
+# likes_received vs www_likes_received
+summary(pf$likes_received)
+summary(pf$www_likes_received)
+
+ggplot(data = pf, aes(x = www_likes_received, y = likes_received)) +
+  geom_point(color = 'black')
+
+ggplot(data = pf, aes(x = www_likes_received, y = likes_received)) +
+  geom_point(color = 'black') +
+  xlim(0, quantile(pf$www_likes_received, .95)) +
+  ylim(0, quantile(pf$likes_received, .95)) +
+  geom_smooth(method = 'lm', color = 'red')
+
+# correlação de 0.948 - não significa que as duas variáveis são tão correlatas. Na verdade esse
+# número é alto porque um conjunto é um subconjunto do outro. (natureza das variáveis)
+with(pf, cor.test(likes_received, www_likes_received))
 
