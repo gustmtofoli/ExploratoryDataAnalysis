02fc89f34eb7199d47e894d967fc9aa290f2c2db
Gustavo Tófoli <gustavo.tofoli@objective.com.br>
2018-03-23 14:51:20 -0300

    test shiny

--- shiny/appR/server.R
+++ shiny/appR/server.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(ggplot2)
+# pf <- read.csv('pseudo_facebook.tsv', sep = '\t')
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -16,13 +17,13 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     
     # generate bins based on input$bins from ui.R
-    # x    <- faithful[, 2] 
+    # x    <- faithful[, 2]
     # bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    data_frame <- data.frame(A = c(15, 16, 17, 18, 19, 20, 21, 22, 23), 
+    data_frame <- data.frame(A = c(15, 16, 17, 18, 19, 20, 21, 22, 23),
                              B = c(10, 20, 30, 40, 80, 50, 60, 70, 15))
     
     # draw the histogram with the specified number of bins
-    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # hist(x, breaks = bins, col = 'blue', border = 'white')
     ggplot(data = data_frame, aes(x = A, y = B)) +
       geom_line(color = 'blue') +
       xlab("age") +
--- shiny/appR/ui.R
+++ shiny/appR/ui.R
@@ -13,21 +13,21 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("First Shiny App Ever"),
+  # titlePanel("First Shiny App Ever"),
   
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-  #   sidebarPanel(
-  #      sliderInput(inputId = "bins",
-  #                  label = "Number of bins:",
-  #                  min = 1,
-  #                  max = 50,
-  #                  value = 30)
-  #   ),
+  # sidebarLayout(
+  # sidebarPanel(
+  #    sliderInput(inputId = "bins",
+  #                label = "Number of bins:",
+  #                min = 1,
+  #                max = 50,
+  #                value = 30)
+  # ),
     
     # Show a plot of the generated distribution
     mainPanel(
        plotOutput(outputId = "distPlot")
     )
-  ))
+  )
 )
